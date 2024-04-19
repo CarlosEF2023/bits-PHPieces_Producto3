3596ffbb5071a18a3e2e3e913e5fe93e9a3b59ce
esteban <etoledob@uoc.edu>
2024-04-19 21:55:36 +0200

    Styles añadidos

--- web/producto3/resources/views/layouts/plantilla.blade.php
+++ web/producto3/resources/views/layouts/plantilla.blade.php
@@ -12,10 +12,10 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/libraries/bootstrap.min.css') }}" id="bootstrap-css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style2.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/wizard.css') }}">
 </head>
 </head>
 
@@ -23,9 +23,11 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     @yield('navbar')
     @yield('content')
     <div class="footer" id="foot" name="foot">bits & PHPieces - Producto 2 - FP064 </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    <script lang="javascript" src="{{ asset('assets/libraries/jquery-3.7.1.min.js') }}"></script>
+    <script lang="javascript" src="{{ asset('assets/libraries/bootstrap.bundle.min.js') }}"></script>
+    <script lang="javascript" src="{{ asset('assets/libraries/popper.min.js') }}"></script>
+    <script lang="javascript" src="{{ asset('assets/libraries/wizard.js') }}"></script>
+
 </body>
 
 </html>
