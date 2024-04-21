d22fc9a26d0f0180fc0f401e92c6916df66dd5d4
CarlosEF2023 <146672623+CarlosEF2023@users.noreply.github.com>
2024-04-21 19:28:51 +0200

    Subiendo index.php para los diferente usuarios
    Subiendo index.php para los diferente usuarios

---
+++ web/producto3/resources/views/vehiculo/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.plantilla')
+@section('title', 'Menú de listado de reservas')
+
+@section("nav")
+@include('layouts.menu_nav')
+@endsection
+
+@section('content')
+<h1>Viajero</h1>
+
+@endsection
