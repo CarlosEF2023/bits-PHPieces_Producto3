cf7017927088b09ecb3783ddda3646af928934d0
CarlosEF2023 <146672623+CarlosEF2023@users.noreply.github.com>
2024-04-23 22:57:15 +0200

    Adaptando a Laravel 10.
    Adaptando a Laravel 10.

--- web/producto3/resources/views/reservas/listados/ver_reservassinconductor.blade.php
+++ web/producto3/resources/views/reservas/listados/ver_reservassinconductor.blade.php
@@ -2,42 +2,42 @@
 @section('title', 'Reservas del Hotel all Aeropuerto')
 
 @section('content')
-<?php
-require_once ("../../controllers/check_connection.php");
-$_SESSION["mensaje"]="";
-include_once "../header.php";
-include_once "../menu.php";
-echo "<br><div class='container-fluid'>";
-echo "<table class='table table-bordered table-striped'>";
-echo "<thead>";
-echo "<tr>";
-echo "<th>Localizador</th>";
-echo "<th>Tipo reserva</th>";
-echo "<th>usuario</th>";
-echo "<th>Fecha reserva</th>";
-echo "<th>Fecha Entrada</th>";
-echo "<th>Hora Entada</th>";
-echo "<th>Hotel</th>";
-echo "<th>Fecha Salida</th>";
-echo "<th>Hora Salida</th>";
-echo "<th>Asignar<br>Conductor</th>";
-echo "</thead>";
-echo "<tbody>";
-$datos= json_decode($_GET['dat'], true);
-foreach ($datos AS $info){
-    echo "<tr>";
-    echo "<td>".$info["localizador"]."</td>";
-    echo "<td>".$info["Descripción"]."</td>";
-    echo "<td>".$info["email_cliente"]."</td>";
-    echo "<td>".$info["fecha_reserva"]."</td>";
-    echo "<td>".$info["fecha_entrada"]."</td>";
-    echo "<td>".$info["hora_entrada"]."</td>";
-    echo "<td>".$info["NombreHotel"]."</td>";
-    echo "<td>".$info["fecha_vuelo_salida"]."</td>";
-    echo "<td>".$info["hora_vuelo_salida"]."</td>";
-    echo "<td><a href='#' class='asignarconductor' id='asigna_conductor' name='asigna_conductor' data=>Asignar conductor</td>";
-    echo "</tr>";
-}
-echo "</tbody>";
-?>
+
+<div class="container-fluid">
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>Localizador</th>
+                <th>Tipo reserva</th>
+                <th>Usuario</th>
+                <th>Fecha reserva</th>
+                <th>Fecha Entrada</th>
+                <th>Hora Entrada</th>
+                <th>Hotel</th>
+                <th>Fecha Salida</th>
+                <th>Hora Salida</th>
+                <th>Asignar<br>Conductor</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($datos as $info)
+            <tr>
+                <td>{{ $info["localizador"] }}</td>
+                <td>{{ $info["Descripción"] }}</td>
+                <td>{{ $info["email_cliente"] }}</td>
+                <td>{{ $info["fecha_reserva"] }}</td>
+                <td>{{ $info["fecha_entrada"] }}</td>
+                <td>{{ $info["hora_entrada"] }}</td>
+                <td>{{ $info["NombreHotel"] }}</td>
+                <td>{{ $info["fecha_vuelo_salida"] }}</td>
+                <td>{{ $info["hora_vuelo_salida"] }}</td>
+                <td>
+                    <a href="#" class="asignarconductor" id="asigna_conductor" name="asigna_conductor" data="">Asignar conductor</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 @endsection
