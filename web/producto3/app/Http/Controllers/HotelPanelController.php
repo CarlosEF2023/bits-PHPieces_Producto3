c84e6ab2bc7a98a4d5cc25f6a36fb6a2f87411b2
CarlosEF2023 <cespigol@uoc.edu>
2024-05-01 09:20:13 +0200

    cambio en panel de Hotel.
    cambio en panel de Hotel.

--- web/producto3/app/Http/Controllers/HotelPanelController.php
+++ web/producto3/app/Http/Controllers/HotelPanelController.php
@@ -9,7 +9,10 @@ class HotelPanelController extends Controller
     public function index()
     {     
         log::channel('mylog')->info('Pasando por HotelPanelController');
+<<<<<<< Updated upstream
         // return "hotelPanel";
+=======
+>>>>>>> Stashed changes
         return view('hotel.index');       
     }
 }
